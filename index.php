6c87a6c74383ea03e2e4e84388f950456f88bd33
(Falcone) <(netofalcone@gmail.com)>
2018-04-22 23:13:48 -0300

    t1.3

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php require 'pages/header.php'; ?>
+<?php
+
+?>
+
+<div class="container-fluid">
+	<div class="jumbotron">
+		<h2>Nós temos hoje x anúncios.</h2>
+		<p>E mais de y usuários cadastrados.</p>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-3">
+			<h4>Pesquisa Avançada</h4>
+			<form method="GET">
+				Filtros
+			</form>
+        </div>
+		<div class="col-sm-9">
+			<h4>Últimos Anúncios</h4>
+			<table class="table table-striped">
+				Lista de últimos anuncios
+				
+			</table>
+		</div>
+	</div>
+
+
+</div>
+
+<?php require 'pages/footer.php'; ?>
