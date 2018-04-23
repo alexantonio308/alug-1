bea2098e758e148e30745919a6d3b0796e7f29c3
(Falcone) <(netofalcone@gmail.com)>
2018-04-23 14:44:27 -0300

    t4.1

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+global $pdo;
+try {
+	$pdo = new PDO("mysql:dbname=bdalug;host=127.0.0.1", "root", "");
+} catch(PDOException $e) {
+	echo "FALHOU: ".$e->getMessage();
+	exit;
+}
+?>
