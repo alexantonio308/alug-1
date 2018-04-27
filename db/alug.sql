d12ae0f4cb891d48103725c1bdb9d9933dc2ded6
Ewerton Coutinho <ewertonmacielcoutinho@gmail.com>
2018-04-27 20:19:43 -0300

    sql

--- db/alug.sql
+++ db/alug.sql
@@ -1,65 +1,60 @@
-CREATE TABLE `anuncios` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`id_usuario` int(11) NOT NULL,
-`id_categoria` int(11) NOT NULL,
-`titulo` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-`descricao` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-`valor` float NULL DEFAULT NULL,
-`estado` int(11) NULL DEFAULT NULL,
-PRIMARY KEY (`id`) 
-)
-ENGINE = InnoDB
-AUTO_INCREMENT = 10
-AVG_ROW_LENGTH = 0
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci
-KEY_BLOCK_SIZE = 0
-MAX_ROWS = 0
-MIN_ROWS = 0
-ROW_FORMAT = Compact;
-CREATE TABLE `anuncios_imagens` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`id_anuncio` int(11) NOT NULL,
-`url` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-PRIMARY KEY (`id`) 
-)
-ENGINE = InnoDB
-AUTO_INCREMENT = 12
-AVG_ROW_LENGTH = 0
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci
-KEY_BLOCK_SIZE = 0
-MAX_ROWS = 0
-MIN_ROWS = 0
-ROW_FORMAT = Compact;
-CREATE TABLE `categorias` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`nome` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-PRIMARY KEY (`id`) 
-)
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-AVG_ROW_LENGTH = 0
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci
-KEY_BLOCK_SIZE = 0
-MAX_ROWS = 0
-MIN_ROWS = 0
-ROW_FORMAT = Compact;
-CREATE TABLE `usuarios` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`nome` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-`email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-`senha` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-`telefone` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-PRIMARY KEY (`id`) 
-)
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-AVG_ROW_LENGTH = 0
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci
-KEY_BLOCK_SIZE = 0
-MAX_ROWS = 0
-MIN_ROWS = 0
-ROW_FORMAT = Compact;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for anuncios
+-- ----------------------------
+DROP TABLE IF EXISTS `anuncios`;
+CREATE TABLE `anuncios`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `titulo` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `descricao` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `valor` float NULL DEFAULT NULL,
+  `estado` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for anuncios_imagens
+-- ----------------------------
+DROP TABLE IF EXISTS `anuncios_imagens`;
+CREATE TABLE `anuncios_imagens`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_anuncio` int(11) NOT NULL,
+  `url` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for categorias
+-- ----------------------------
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE `categorias`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of categorias
+-- ----------------------------
+INSERT INTO `categorias` VALUES (1, 'Apartamento Padrão');
+INSERT INTO `categorias` VALUES (2, 'Casa de Condomínio');
+INSERT INTO `categorias` VALUES (3, 'Casa Padrão');
+INSERT INTO `categorias` VALUES (4, 'Hotel');
+
+-- ----------------------------
+-- Table structure for usuarios
+-- ----------------------------
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `senha` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `telefone` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+ROW_FORMAT = Dynamic;
