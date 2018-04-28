7fc1fe1cb522ca77faf574c249f2502b2fb4f5b4
Ewerton Coutinho <ewertonmacielcoutinho@gmail.com>
2018-04-27 22:35:42 -0300

    t1.1

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php require 'pages/header.php'; ?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-3">
+			<h4>Pesquisa Avançada</h4>
+		</div>
+		<div class="col-sm-9">
+			<h4>Últimos Anúncios</h4>
+			<table class="table table-striped">
+				<tbody>
+					<tr>
+						<td>
+							<img src="assets/images/default.jpg" height="50" border="0">
+						</td>
+                        <td>
+                            <a href="">Jandim Bougainville</a><br>
+                            Apartamento Padrão
+                        </td>
+                        <td>R$ 192,000.00</td>
+                    </tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<?php require 'pages/footer.php'; ?>
