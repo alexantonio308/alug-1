9bcb1f1004abbb700b1a83ac22e6d5b3197e2beb
Lucas Aragão <lucas.magno@dce.ufpb.br>
2018-05-13 03:13:27 -0300

    anuncios cadastrados no front

--- index.php
+++ index.php
@@ -1,6 +1,46 @@
 <?php require 'pages/header.php'; ?>
 
+
+<?php
+require 'classes/anuncios.class.php';
+require 'classes/usuarios.class.php';
+require 'classes/categorias.class.php';
+$a = new Anuncios();
+$u = new Usuarios();
+$c = new Categorias();
+
+$filtros = array(
+	'categoria' => '',
+	'preco' => '',
+	'estado' => ''
+);
+if(isset($_GET['filtros'])) {
+	$filtros = $_GET['filtros'];
+}
+
+$total_anuncios = $a->getTotalAnuncios($filtros);
+$total_usuarios = $u->getTotalUsuarios();
+
+$p = 1;
+if(isset($_GET['p']) && !empty($_GET['p'])) {
+	$p = addslashes($_GET['p']);
+}
+
+$por_pagina = 2;
+$total_paginas = ceil($total_anuncios / $por_pagina);
+
+$anuncios = $a->getUltimosAnuncios($p, $por_pagina, $filtros);
+$categorias = $c->getLista();
+?>
+
+
 <div class="container-fluid">
+	<div class="jumbotron">
+		<h2>Nós temos hoje <?php echo $total_anuncios; ?> anúncios.</h2>
+		<p>E mais de <?php echo $total_usuarios; ?> usuários cadastrados.</p>
+	</div>
+
+
 	<div class="row">
 		<div class="col-sm-3">
 			<h4>Pesquisa Avançada</h4>
